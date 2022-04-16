6b2b8d799aff8c8d6125555d7d784d4d5c86ab3d
Siteri David <david.siteri@evosoft.com>
2022-04-16 20:12:38 +0200

    Added Animal and Breed models

---
+++ Shelter/Models/Animal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Models
+{
+  public class Animal
+  {
+    /// <summary>
+    /// Sex of the Animal. It can only be male or female.
+    /// </summary>
+    public enum Sex
+    {
+      /// <summary>
+      /// Male animal.
+      /// </summary>
+      Male,
+
+      /// <summary>
+      /// Female animal.
+      /// </summary>
+      Female,
+    }
+
+    /// <summary>
+    /// Gets or sets unique identificator of the animal.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets name of the animal.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets sex of the animal.
+    /// </summary>
+    public Sex MF { get; set; }
+
+    /// <summary>
+    /// Gets or sets age of the animal.
+    /// </summary>
+    public int Age { get; set; }
+
+    /// <summary>
+    /// Gets or sets id of the breed, where the animal belongs.
+    /// </summary>
+    public int BreedId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the breed of the animal.
+    /// </summary>
+    public virtual Breed Breed { get; set; }
+
+    /// <summary>
+    /// ToString override.
+    /// </summary>
+    /// <returns>A formatted string which contains all the relevant information about the animal.</returns>
+    public override string ToString()
+    {
+      return $"[{this.Id}] Name: {this.Name}, Age: {this.Age}, Sex: {this.MF}, BreedId: {this.BreedId}, Breed: {this.Breed.Name}";
+    }
+  }
+}
---
+++ Shelter/Models/Breed.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+  public class Breed
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Breed"/> class.
+    /// </summary>
+    public Breed()
+    {
+      this.Animals = new HashSet<Animal>();
+    }
+
+    /// <summary>
+    /// Gets or sets unique identificator of the breed.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets name of the breed.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets collection of animal entities in the breed.
+    /// </summary>
+    public virtual ICollection<Animal> Animals { get; set; }
+
+    /// <summary>
+    /// ToString override.
+    /// </summary>
+    /// <returns>A formatted string which contains all the relevant information about the breed.</returns>
+    public override string ToString()
+    {
+      return $"Name: {this.Name}";
+    }
+  }
+}
