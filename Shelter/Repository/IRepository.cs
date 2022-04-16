147bdcb16dfbc6faf13b569499abd31f2955e6d6
Siteri David <david.siteri@evosoft.com>
2022-04-16 20:20:16 +0200

    Added Repository CRUD methods and installed necessary nuGet packages

---
+++ Shelter/Repository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Repository
+{
+  public interface IRepository<T>
+      where T : class
+  {
+    /// <summary>
+    /// Gets one entity.
+    /// </summary>
+    /// <param name="id">ID of the entity.</param>
+    /// <returns>The specifited entity.</returns>
+    T GetOne(int id);
+
+    /// <summary>
+    /// Gets all entity.
+    /// </summary>
+    /// <returns>All the entities.</returns>
+    IQueryable<T> GetAll();
+
+    // void Insert(T entity);
+    // bool Remove(T entity / int id);
+  }
+}
---
+++ Shelter/Repository/Repository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+  public abstract class Repository<T> : IRepository<T>
+          where T : class
+  {
+    /// <summary>
+    /// Dbcontext.
+    /// </summary>
+    protected DbContext ctx;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Repository{T}"/> class.
+    /// </summary>
+    /// <param name="ctx">Dbcontext.</param>
+    protected Repository(DbContext ctx)
+    {
+      this.ctx = ctx;
+    }
+
+    /// <inheritdoc/>
+    public IQueryable<T> GetAll()
+    {
+      return this.ctx.Set<T>();
+    }
+
+    /// <inheritdoc/>
+    public abstract T GetOne(int id);
+  }
+}
